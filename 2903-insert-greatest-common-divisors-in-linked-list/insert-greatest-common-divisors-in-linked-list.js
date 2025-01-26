6a7e0806956be5254bd043c9cc96ea7ad4392d20
Brijbhan Chauhan <34192397+brij2020@users.noreply.github.com>
2025-01-26 21:37:44 +0530

    Time: 6 ms (45.73%) | Memory: 57.9 MB (96.58%) - LeetSync

---
+++ 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.js
@@ -0,0 +1,59 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const gcd = (num1, num2) => {
+
+    const __inner = (n1,n2) => {
+        if(n1%n2 == 0) return n2
+        return __inner(n2, n1%n2)
+
+    }
+
+    if(num1 > num2) {
+        return __inner(num1, num2)
+    } else {
+        return __inner(num2, num1)
+
+    }
+
+}
+var insertGreatestCommonDivisors = function(head) {
+    
+    let c = head
+    let prev = null
+    while(c) {
+
+        if(prev !==null) {
+            let intermediat = gcd(prev.val, c.val);
+            let n = new  ListNode(intermediat);
+            prev.next = n;
+            n.next = c;
+
+        }
+
+        prev = c;
+        c = c.next;
+    }
+    return head
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
